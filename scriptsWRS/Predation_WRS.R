158266b4a7dd6e8f7c8240a48060c41c8a2a1b9a
Jared Brewer <jaredbrewer@me.com>
2015-07-30 16:33:03 -0400

    Ditto.

--- scriptsWRS/Predation_WRS.R
+++ scriptsWRS/Predation_WRS.R
@@ -15,3 +15,9 @@ pred.melt <- melt(pred)
 wilcox.test(value ~ variable, data=pred.melt)
 
 ggplot(pred.melt, aes(x=variable, y=value, fill=variable)) + geom_boxplot() + xlab("Experimental Groups") + ylab("CFU/mL")+ scale_fill_manual(values=c("darkorchid4", "white"), name="Phenotype", labels=c("Purple", "White"))  + geom_blank() 
+
+mixpred <- read.csv("Predation_07302015_2_WJB.csv", header = T)
+mixpred.melt <- melt(mixpred)
+wilcox.test(value ~ variable, data=mixpred.melt)
+
+ggplot(mixpred.melt, aes(x=variable, y=value, fill=variable)) + geom_boxplot() + xlab("Experimental Groups") + ylab("CFU/mL")+ scale_fill_manual(values=c("darkorchid4", "white"), name="Phenotype", labels=c("Purple", "White"))  + geom_blank() 
